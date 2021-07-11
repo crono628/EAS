2ed2fc57cb84cb3a75375b3ff08c3be2a79ca65e
Michael DeSantis <m.desantis628@gmail.com>
2021-07-11 19:03:15 -0400

    commit on main

---
+++ EAS.js
@@ -0,0 +1,10 @@
+const div = document.createElement('div')
+const container = document.getElementsByClassName("container")
+
+function mainGrid {
+    let gridSize = num * num;
+    for (let i = 1; i <= gridSize; i++){
+        
+    }
+
+}
