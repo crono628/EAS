950d4dc006364f2b735aa80543bff751513198b8
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-13 16:56:48 +0000

    testing

--- EAS.js
+++ EAS.js
@@ -3,15 +3,26 @@ const container = document.getElementsByClassName("container")
 const button = document.querySelector('button')
 button.addEventListener('click', updateGrid())
 
-function updateGrid(size) {
-    let x = document.getElementById("input")
-    for (let i = 1; i <= x * x; i++)
-    if (size.input[i])
-    
-}
-console.log(button)
-console.log(updateGrid)
+// function createGrid(size) {
+//   for (let index = 0; index < size * size; index++) {
+//     let div = document.createElement('div')
+//     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+//     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+//     grid.appendChild(div).className = 'div'
+//   }
 
+//   // changes color of grid when hovered
+//   let squares = document.querySelectorAll('.div')
+//   squares.forEach(square => {
+//     square.addEventListener('mouseover', () => {
+//       let randomColor = `rgb(${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)})`
+//       square.style.backgroundColor = randomColor
+//     })
+//   })
+// }
+
+createGrid(10)
+console.log(createGrid)
 // function mainGrid(x) {
 //     let gridSize = num * num;
 //     for (let num = 4; num <= gridSize; num++){
@@ -20,3 +31,4 @@ console.log(updateGrid)
 //     }
 // }
 
+console.log(button)
