3bc8b508c0bbc784ebf273db652c355ccd993aec
Michael DeSantis <m.desantis628@gmail.com>
2021-07-12 20:49:26 -0400

    commit on main

--- EAS.js
+++ EAS.js
@@ -1,10 +1,22 @@
 const div = document.createElement('div')
 const container = document.getElementsByClassName("container")
+const button = document.querySelector('button')
+button.addEventListener('click', updateGrid())
 
-function mainGrid {
-    let gridSize = num * num;
-    for (let i = 1; i <= gridSize; i++){
-        
-    }
-
+function updateGrid(size) {
+    let x = document.getElementById("input")
+    for (let i = 1; i <= x * x; i++)
+    if (size.input[i])
+    
 }
+console.log(button)
+console.log(updateGrid)
+
+// function mainGrid(x) {
+//     let gridSize = num * num;
+//     for (let num = 4; num <= gridSize; num++){
+//         if (gridSize[num])
+//         return x
+//     }
+// }
+
