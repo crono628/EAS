4da4dfdc7920916307ea8c82736d79f42f02c7ba
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-13 18:40:31 +0000

    commit for studying only

--- EAS.js
+++ EAS.js
@@ -1,34 +1,97 @@
-const div = document.createElement('div')
-const container = document.getElementsByClassName("container")
-const button = document.querySelector('button')
-button.addEventListener('click', updateGrid())
+const colorPicker = new iro.ColorPicker("#picker", {width: 170, color: "#FF006E" });
+const gridContainer = document.querySelector('.grid-container');
+const clearGridButton = document.querySelector('.clear-button');
+const toolsGridButton = document.querySelector('.tools-button');
+const penTypeButton = document.getElementById('pen-type');
+const fillGridButton = document.getElementById('fill-grid');
+const rainbowButton = document.getElementById('rainbow');
 
-// function createGrid(size) {
-//   for (let index = 0; index < size * size; index++) {
-//     let div = document.createElement('div')
-//     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-//     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-//     grid.appendChild(div).className = 'div'
-//   }
 
-//   // changes color of grid when hovered
-//   let squares = document.querySelectorAll('.div')
-//   squares.forEach(square => {
-//     square.addEventListener('mouseover', () => {
-//       let randomColor = `rgb(${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)})`
-//       square.style.backgroundColor = randomColor
-//     })
-//   })
-// }
+let startSize = 16 ;
+let gridWidthHeight = 41 ;
+let penType = 0 ;
+var first = 'false';
+//const penStroke = ['mouseOver' , 'click'];
 
-createGrid(10)
-console.log(createGrid)
-// function mainGrid(x) {
-//     let gridSize = num * num;
-//     for (let num = 4; num <= gridSize; num++){
-//         if (gridSize[num])
-//         return x
-//     }
-// }
+function createGrid(size){
+    if(first !== 'false'){resetBoard();}
+    for(var i = 1 ; i <= (size*size) ; i++){
+        var newDiv = document.createElement('div');
+        newDiv.classList.add('grid-div');
+        gridContainer.appendChild(newDiv); 
+    }
+    gridCell = document.querySelectorAll(".grid-div");
+    gridCell.forEach(cell => {
+        cell.style.width  = `${gridWidthHeight / size}rem`;
+        cell.style.height = `${gridWidthHeight / size}rem`;
+    }) 
+    changePenType();
+    first = true;
+}
 
-console.log(button)
+function changeBackground(){
+    this.style.backgroundColor = gridColor ;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".grid-container");
+    while (board.firstChild){
+        board.removeChild(board.firstChild);
+    }  
+}
+
+function clearBoardColor() {
+    for(var i = 0 ; i < gridCell.length ; i++){
+        gridCell[i].style.backgroundColor='white';
+    }
+}
+
+function fillBoardColor() {
+    for(var i = 0 ; i < gridCell.length ; i++){
+        gridCell[i].style.backgroundColor = gridColor;
+    }
+}
+
+function changePenType(){
+    if (penType == 1 ){
+        document.getElementById('pen-type').textContent = 'Pen Type: Click' ;
+        gridCell.forEach(cell => {
+            cell.removeEventListener('mouseover',changeBackground);
+            cell.addEventListener('click' , changeBackground);
+         })
+        --penType;
+    }
+    else{
+        document.getElementById('pen-type').textContent = 'Pen Type: Follow' ;
+        gridCell.forEach(cell => {
+            cell.removeEventListener('click',changeBackground);
+            cell.addEventListener('mouseover' , changeBackground);
+         })
+        ++penType;
+    }
+}
+
+function rainbowColor(){
+    
+}
+
+function toogleToolsWindow(){
+    if (document.getElementById('tools').style.visibility == 'hidden'){
+        document.getElementById('tools').style.visibility = 'visible';
+    } 
+    else {
+        document.getElementById('tools').style.visibility = 'hidden' ;
+    }
+}
+
+createGrid(startSize);
+
+// listen to a color picker's color:init and color:change events
+colorPicker.on(['color:init', 'color:change'], function(color) {gridColor = color.hexString;});
+
+// ACTIVE LISTENERS //
+clearGridButton.addEventListener('click', clearBoardColor);
+toolsGridButton.addEventListener('click', toogleToolsWindow);
+fillGridButton.addEventListener('click', fillBoardColor);
+penTypeButton.addEventListener('click', changePenType);
+//rainbowButton.addEventListener('click,' rainbowColor);
