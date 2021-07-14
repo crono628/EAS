c60b461e2a00d728370be2b142b3621d40cef174
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-14 19:57:18 +0000

    commit on main

--- EAS.js
+++ EAS.js
@@ -1,16 +1,44 @@
-let container = document.querySelector("container")
-let div = document.createElement('div')
-div.classList.add('grid')
+const container = document.querySelector(".container")
+const btnClear = document.querySelector('#clear');
+btnClear.onclick = () => clearGrid();
+const chartSize = document.querySelector("#chartSize").value
+chartSize.onclick = () =>
 
-
-function drawRows(x) {
-let gridCells = x * x
-for (let i = 0; i < gridCells; i++){
-    
+function newChartSize (){
+  chartSize.append
 }
 
+originalGrid = () => {
+  for (let i = 0; i < (chartSize * chartSize); i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('grid');
+    newDiv.addEventListener('mouseover', function (event) {
+      event.target.style.backgroundColor = 'black';
+    })
+    container.appendChild(newDiv);
+  }
+};
 
+originalGrid()
 
+function updateGrid() {
+  for (let i = 0; i < (chartSize * chartSize); i++) {
+    if ((x < 4) || (x > 16)) {
+      x = prompt('choose a number between 4 and 16');
+    } else {
+      const newDiv = document.createElement('div');
+      newDiv.classList.add("grid");
+      newDiv.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = 'black';
+      });
+      container.appendChild(newDiv)
+    }
+  }
+}
 
-drawRows(10)
-console.log(drawRows(10))
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  originalGrid();
+}
