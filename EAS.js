e773630678ecba219a804e11f07caec632132ed7
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-15 20:16:46 +0000

    slider

--- EAS.js
+++ EAS.js
@@ -1,38 +1,18 @@
 const container = document.querySelector(".container")
 const btnClear = document.querySelector('#clear');
 btnClear.onclick = () => clearGrid();
-const chartSize = document.querySelector("#chartSize").value
-chartSize.onclick = () =>
 
-function newChartSize (){
-  chartSize.append
-}
 
-originalGrid = () => {
-  for (let i = 0; i < (chartSize * chartSize); i++) {
+function originalGrid() {
+  let gridSize = document.getElementById('slider').value;
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('grid');
     newDiv.addEventListener('mouseover', function (event) {
       event.target.style.backgroundColor = 'black';
-    })
+
+    });
     container.appendChild(newDiv);
-  }
-};
-
-originalGrid()
-
-function updateGrid() {
-  for (let i = 0; i < (chartSize * chartSize); i++) {
-    if ((x < 4) || (x > 16)) {
-      x = prompt('choose a number between 4 and 16');
-    } else {
-      const newDiv = document.createElement('div');
-      newDiv.classList.add("grid");
-      newDiv.addEventListener('mouseover', function (event) {
-        event.target.style.backgroundColor = 'black';
-      });
-      container.appendChild(newDiv)
-    }
   }
 }
 
@@ -42,3 +22,6 @@ function clearGrid() {
   }
   originalGrid();
 }
+
+
+
