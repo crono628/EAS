e6da7892034ebe82637c2483b62f98904866a619
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-16 14:10:36 +0000

    live grid working with slider

--- EAS.js
+++ EAS.js
@@ -1,33 +1,32 @@
 const container = document.querySelector(".container")
+const gridInfo = document.querySelector("#gridInfo")
 const btnClear = document.querySelector('#clear');
 btnClear.onclick = () => clearGrid();
-let gridVal = document.getElementById('slider').value;
-const gridInfo = document.getElementById("gridInfo")
-gridInfo.textContent = `GRID SIZE:${gridVal}`
 
 originalGrid()
 
 function originalGrid() {
-  let gridSize = document.getElementById('slider').value;
+  let gridSize = document.querySelector('#slider').value;
   container.style.cssText = `grid-template-columns: repeat(${gridSize}, 2fr); grid-template-rows: repeat(${gridSize}, 2fr);`
   for (let i = 0; i < gridSize * gridSize; i++) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('grid');
+    newDiv.setAttribute('id', 'grid');
     newDiv.addEventListener('mouseover', function (event) {
       event.target.style.backgroundColor = 'black';
     });
     container.appendChild(newDiv);
   }
-
-
-  let slider = document.getElementsByClassName("grid")
-  slider.addEventListener('input', function () {
-    clearGrid();
-    let gridSize = document.getElementById('slider').value;
-    originalGrid(gridSize * gridSize);
-    container.style.cssText = `grid-template-columns: repeat(${gridSize}, 2fr); grid-template-rows: repeat(${gridSize}, 2fr);`
-  })
 }
+
+
+const slider = document.querySelector("#slider")
+gridInfo.textContent = `GRID SIZE: ${slider.value} x ${slider.value}`
+slider.oninput = function () {
+  gridInfo.textContent = `GRID SIZE: ${slider.value} x ${slider.value}`
+  originalGrid();
+  clearGrid()
+}
+
 
 function clearGrid() {
   while (container.firstChild) {
