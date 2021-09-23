ad73da047ce0b997da36ae4e3b7c350750f6049c
Michael DeSantis <m.desantis628@gmail.com>
2021-09-22 22:44:57 -0400

    functioning with black background

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-const container = document.querySelector('.container')
-const slideIt = document.querySelector('.slider')
+const resetBtn = document.querySelector('.reset')
+const toggleBtn = document.querySelector('.toggle-grid')
+const btnDiv = document.querySelector('.btn-div')
 
-(function bigSlide() {
-  let cellValue = slideIt.value
+function bigSlide() {
+  const container = document.querySelector('.container')
+  let cellValue = document.querySelector('.slider').value
+  let display = document.querySelector('.size-display')
+  display.textContent = `${cellValue} x ${cellValue}`
   container.style.cssText = `grid-template-columns: repeat(${cellValue}, 2fr); grid-template-rows: repeat(${cellValue}, 2fr);`
   for (let i = 0; i < cellValue * cellValue; i++) {
     const newDiv = document.createElement('div')
     newDiv.classList.add('cell')
-    newDiv.style.border = 'solid black 2px'
+    newDiv.classList.add('border')
+    newDiv.addEventListener('mouseenter', () => {
+      newDiv.style.backgroundColor = 'black'
+    })
     container.appendChild(newDiv)
   }
-})()
 
+}
+
+resetBtn.onclick = () => clearGrid()
+toggleBtn.onclick = () => gridBeGone()
+
+function clearGrid() {
+  const container = document.querySelector('.container')
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  bigSlide();
+}
+
+function gridBeGone() {
+  let allCells = document.querySelectorAll('.cell')
+  cellArray = [...allCells]
+  cellArray.forEach(cell => {
+    cell.classList.toggle('border')
+  });
+}
+
+let slideIt = document.querySelector('.slider')
+slideIt.oninput = function () {
+  bigSlide()
+  clearGrid()
+}
+
+// let gridCell = document.querySelectorAll('.cell')
+// gridCell.forEach((item) => {
+//   item.addEventListener('mouseover', () => {
+//     item.style.backgroundColor = 'black'
+//   })
+// })
+
+bigSlide()
 console.log(cellValue);
